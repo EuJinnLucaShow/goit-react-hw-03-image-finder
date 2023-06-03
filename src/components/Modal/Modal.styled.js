88102b97e6458208a57aab0eb47a1e4fbe77e964
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-03 15:25:58 +0300

    Fix styles

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -14,6 +14,6 @@ export const Overlay = styled.div`
 `;
 
 export const ModalDiv = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  max-width: 130vh;
+  max-height: 60vw;
 `;
