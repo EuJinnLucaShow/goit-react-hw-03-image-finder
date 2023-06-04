f402f9d57a5f4eccfb37c99c88c907eb51ce5cd3
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-04 19:14:29 +0300

    Update Loader.styled.js

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,7 +1,16 @@
   import styled from "styled-components";
 
 export const LoadDiv = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
 `
